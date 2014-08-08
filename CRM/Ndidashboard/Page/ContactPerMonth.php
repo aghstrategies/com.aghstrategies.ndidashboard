5ace78bbe75c09f56133e89c70ec02f64d444063
Maggie <maggie.epps@gmail.com>
2014-08-08 17:55:17 -0400

    Mostly working except setting dashlet to default and minor appearance tweaks

--- CRM/Ndidashboard/Page/ContactPerMonth.php
+++ CRM/Ndidashboard/Page/ContactPerMonth.php
@@ -8,10 +8,12 @@ class CRM_Ndidashboard_Page_ContactPerMonth extends CRM_Core_Page {
       );
     $createdArray = array();
     foreach ($date_ranges as $name => $date_range){
-      $sql = "SELECT count(id) as createdlastmonth FROM civicrm_contact WHERE MONTH(created_date) = MONTH(CURRENT_DATE ".$date_range.") AND YEAR(created_date) = YEAR(CURRENT_DATE ".$date_range.");";
+      // $sql = "SELECT count(id) as createdtotal, DATE_FORMAT(MONTH(CURRENT_DATE ".$date_range."), '%M') as month  FROM civicrm_contact WHERE MONTH(created_date) = MONTH(CURRENT_DATE ".$date_range.") AND YEAR(created_date) = YEAR(CURRENT_DATE ".$date_range.");";
+  $sql = "SELECT count(id) as createdtotal  FROM civicrm_contact WHERE MONTH(created_date) = MONTH(CURRENT_DATE ".$date_range.") AND YEAR(created_date) = YEAR(CURRENT_DATE ".$date_range.");";
+
       $dao = CRM_Core_DAO::executeQuery($sql);
       if ($dao->fetch()){
-        $createdArray[$name] = $dao->createdlastmonth;
+        $createdArray[$name] = $dao->createdtotal;
       }
     }
   $createdAgg = array();
@@ -19,9 +21,9 @@ class CRM_Ndidashboard_Page_ContactPerMonth extends CRM_Core_Page {
   foreach ($createdArray as $name => $created){
     if ($createdTotal<50)
       $createdAgg[$name] = $created;
-    elseif ($createdTotal < 300 && $createdTotal >50)
+    elseif ($createdTotal < 300 && $createdTotal >=50)
       $createdAgg[$name] = $created/10;
-    elseif ($createdTotal < 1000 && $createdTotal >300)
+    elseif ($createdTotal < 1000 && $createdTotal >=300)
       $createdAgg[$name] = $created/100;
     else
       $createdAgg[$name] = $created/1000;
--- ndidashboard.php
+++ ndidashboard.php
@@ -13,45 +13,117 @@ function ndidashboard_civicrm_pageRun() {
 /**
  * Implementation of hook_civicrm_pageRun
  */
-function ndidashboard_civicrm_defaults($availableDashlets, &$defaultDashlets){
+function ndidashboard_civicrm_dashboard_defaults($availableDashlets, &$defaultDashlets){
+$contactID = CRM_Core_Session::singleton()->get('userID');
   try{
-   $dashlet = civicrm_api3('Dashboard', 'get', array(
-      'name'  =>  'contact_per_month',
+   $dashlet = civicrm_api3('DashboardContacat', 'getSingle', array(
+      'dashboard_id'  =>  $availableDashlets['contact_per_month']['id'],
+      'contact_id' => $contactID,
    ));
 }
 catch (CiviCRM_API3_Exception $e) {
    $error = $e->getMessage();
 }
-    $contactID = CRM_Core_Session::singleton()->get('userID');
-   $defaultDashlets[] = array(
-    'dashboard_id' => $dashlet['id'],
-    'is_active' => 1,
-    'column_no' => 1,
-    'contact_id' => $contactID,
-   );
- }
 
+// $defaultDashlets[] = array(
+//   'id' => $dashlet['id'],
+//     "dashboard_id" => $availableDashlets['contact_per_month']['id'],
+//     "contact_id" => $contactID,
+//     "column_no" => "1",
+// // //     "is_minimized" => "0",
+// // //     "is_fullscreen" => "1",
+//     "is_active" => "1",
+// //     "weight" => "0",
+   // );
+ }
 
 /**
  * Implementation of hook_civicrm_dashboard
  */
 function ndidashboard_civicrm_dashboard( $contactID, &$contentPlacement ) {
+//   try{
+//    $dashlet = civicrm_api3('Dashboard', 'getSingle', array(
+//       'name'  =>  'contact_per_month',
+//    ));
+//   }
+//   catch (CiviCRM_API3_Exception $e) {
+//      $error = $e->getMessage();
+//   }
+//   $contactID = CRM_Core_Session::singleton()->get('userID');
+//     try{
+//    $contactDashlet = civicrm_api3('DashboardContact', 'getSingle', array(
+//       'dashboard_id'  =>  $dashlet['id'],
+//       'contact_id' => $contactID,
+//    ));
+// }
+// catch (CiviCRM_API3_Exception $e) {
+//    $error = $e->getMessage();
+// }
+
+  // $params = array(
+  //   "id" => $contactDashlet['id'],
+  //   "contact_id" => $contactID,
+  //   "column_no" => "1",
+  //   "dashboard_id" => $dashlet['id'],
+  //   // "is_minimized" => "0",
+  //   // "is_fullscreen" => "1",
+  //   "is_active" => "1",
+  //   // "weight" => "0",
+  //   );
+  // civicrm_api3('DashboardContact', 'Create', $params);
+  // $params = array(
+  //   "dashboard_id" => 2,
+  //   "contact_id" => $contactID,
+  //   "column_no" => "0",
+  //   "is_minimized" => "0",
+  //   "is_fullscreen" => "1",
+  //   "is_active" => "1",
+  //   "weight" => "0",
+  //   );
+  // civicrm_api3('DashboardContact', 'Create', $params);
+
+  //Communication
+  $sendMailing = CRM_Utils_System::url('civicrm/mailing/send', $query = 'reset=1' );
+
+  //Manage Contacts
   $newIndLink = CRM_Utils_System::url('civicrm/contact/add', $query = 'reset=1&ct=Individual' );
+  $browseContacts = CRM_Utils_System::url('civicrm/contact/search', $query = 'reset=1&force=1' );
   $manageGroupLink = CRM_Utils_System::url('civicrm/group', $query = 'reset=1' );
+  $viewAllReports = CRM_Utils_System::url('civicrm/report/list', $query = 'reset=1' );
+
+  //Manage Events
+  $newEvent = CRM_Utils_System::url('civicrm/event/add', $query = 'reset=1&action=add' );
+  $manageEvents = CRM_Utils_System::url('civicrm/event/manage', $query = 'reset=1' );
+  $searchParticipants = CRM_Utils_System::url('civicrm/event/search', $query = 'reset=1' );
+  $registerParticipant = CRM_Utils_System::url('civicrm/participant/add', $query = 'reset=1&action=add&context=standalone' );
+  $scheduleReminder = CRM_Utils_System::url('civicrm/admin/scheduleReminders', $query = 'reset=1' );
+
+
+
 
   $contentPlacement =2;
   return array(
  //'<h2>Welcome</h2>' => "<p>Welcome to your CiviCRM Dashboard<p>",
-                  '<h2>Links</h2>' =>
+
+                  '<h2>Contacts</h2>' =>
                     "<p>
                     <a href='".$newIndLink."'><button type=\"button\" class=\"btn btn-primary\">Create New Individual</button></a>
+                    <a href='".$browseContacts."'><button type=\"button\" class=\"btn btn-primary\">Browse Contacts</button></a>
                    <a href='".$manageGroupLink."'><button type=\"button\" class=\"btn btn-primary\">Manage Groups</button></a>
+                    <a href='".$viewAllReports."'><button type=\"button\" class=\"btn btn-primary\">View All Reports</button></a>
+                    <a href='".$sendMailing."'><button type=\"button\" class=\"btn btn-primary\">Send Mailing</button></a>
                     </p>
-
+                  ",
+                  '<h2>Events</h2>' =>
+                    "<p>
+                    <a href='".$newIndLink."'><button type=\"button\" class=\"btn btn-success\">Organize Event</button></a>
+                    <a href='".$manageEvents."'><button type=\"button\" class=\"btn btn-success\">All Events</button></a>
+                   <a href='".$searchParticipants."'><button type=\"button\" class=\"btn btn-success\">Search Participants</button></a>
+                    <a href='".$registerParticipant."'><button type=\"button\" class=\"btn btn-success\">Register Participant</button></a>
+                      <a href='".$scheduleReminder."'><button type=\"button\" class=\"btn btn-success\">Schedule Reminder</button></a>
+                  </p>
                   ",
     );
-//  $print = $page->_embedded;
- // print_r($print);
 }
 
 /**
@@ -119,20 +191,27 @@ function ndidashboard_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
  */
 function ndidashboard_civicrm_managed(&$entities) {
   $entities[] = array(
-    'module' => 'com.example.modulename',
-    'name' => 'myreport',
-    'entity' => 'ReportTemplate',
+    'module' => 'com.aghstrategies.ndidashboard',
+    'name' => 'contactpermonth',
+    'entity' => 'Dashboard',
     'params' => array(
       'version' => 3,
     "domain_id" => "1",
     "name" => "contact_per_month",
-    "label" => "Recently Added Contacts",
+    "label" => "Recently Added",
     "url" => "civicrm/dashlets/contactpermonth&snippet=1",
     "column_no" => "0",
     "is_minimized" => "0",
     "is_fullscreen" => "1",
     "is_active" => "1",
-    "is_reserved" => "0",
+    "is_reserved" => "1",
     "weight" => "0"
     ),
-  );}
+  );
+  // try{
+  //   $ufMatches = civicrm_api3('UFMatch', 'get', array());
+  // }
+  // foreach ($ufMatches as $ufMatch){
+
+  // }
+}
